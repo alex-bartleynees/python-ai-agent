40a2b26f0b838b6c3a1a10e88dd89663a785b776
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-09-20 12:46:11 +1200

    Add files info function

---
+++ functions/get_files_info.py
@@ -0,0 +1,29 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+
+    if not is_subpath(full_path, working_directory):
+        return f"Error: Cannot list {directory} as it is outside the permitted working directory"
+
+    if not os.path.isdir(full_path):
+        return f"Error: {directory} is not a directory"
+
+    directory_contents = ""
+
+    try:
+        for item in os.listdir(full_path):
+            item_path = os.path.join(full_path, item)
+            item_info_string = f"{item}: file_size: {os.path.getsize(item_path)}, is_dir={os.path.isdir(item_path)}"
+            directory_contents += item_info_string + "\n"
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+    return directory_contents.strip()
+
+
+def is_subpath(path, base):
+    path = os.path.abspath(path)
+    base = os.path.abspath(base)
+    return os.path.commonpath([base]) == os.path.commonpath([path, base])
