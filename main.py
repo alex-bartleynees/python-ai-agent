e194d628d19e370d69ee9918641a1b829f97e931
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-09-23 19:07:21 +1200

    Update with system prompt and function tools declarations

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 
 def main():
@@ -15,7 +31,7 @@ def main():
         print("Please provide a prompt as a command line argument.")
         sys.exit(1)
 
-    user_prompt = " ".join(args[0])
+    user_prompt = " ".join(args)
 
     verbose_mode = ""
     if len(args) > 1:
@@ -31,6 +47,9 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt, tools=[available_functions]
+        ),
     )
 
     if verbose_mode:
@@ -38,7 +57,11 @@ def main():
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
